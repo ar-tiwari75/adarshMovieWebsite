fdebb2e820546c47f4f65bfe097147cc2f105c24
ar-tiwari75 <105338363+ar-tiwari75@users.noreply.github.com>
2023-03-23 19:43:59 +0530

    Update contex.js

--- src/contex.js
+++ src/contex.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 
-export const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL = `http://www.omdbapi.com/?apikey=ee80d33c`;
 
 const AppContext = React.createContext();
 
